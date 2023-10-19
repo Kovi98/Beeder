46b56caf20da1aa3f7d326364f62f86e296b55f0
Jan PEKAŘ <jpekar@algotech.cz>
2023-10-19 20:25:09 +0200

    more

--- src/Beeder.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Beeder.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Beeder.Api.Data
+namespace Beeder.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
